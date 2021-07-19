e90c01fe70bc8aad0b7e9c26a01b292a722f6b2c
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-20 02:36:44 +0530

    created objects for Buttons and CameraManger

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingButton.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingButton.java
@@ -2,10 +2,13 @@ package sanskar.kumar.kumarsanskar.torchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UsingButton extends AppCompatActivity {
-
+    Button torchOn,torchOff;
+    CameraManager cameraManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
