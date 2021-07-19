72091fd748792697477534f0e55148758381c454
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-20 02:46:04 +0530

    used setOnClickListener() with torchOn button

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingButton.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingButton.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class UsingButton extends AppCompatActivity {
@@ -17,5 +18,11 @@ public class UsingButton extends AppCompatActivity {
         torchOn = (Button)findViewById(R.id.button4);
         torchOff = (Button)findViewById(R.id.button5);
         cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
+
+        torchOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
