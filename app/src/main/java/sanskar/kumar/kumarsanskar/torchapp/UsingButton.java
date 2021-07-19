dedaba3335189a308a573e753094d439c42a2a5d
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 20:50:33 +0530

    added some empty activities

---
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingButton.java
@@ -0,0 +1,14 @@
+package sanskar.kumar.kumarsanskar.torchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsingButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_using_button);
+    }
+}
