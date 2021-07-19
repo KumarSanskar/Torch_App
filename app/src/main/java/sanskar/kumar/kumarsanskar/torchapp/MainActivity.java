e3450b41494ec9b7fa7868a9fae6c3e41ad88a3e
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-20 02:26:58 +0530

    used setOnClickListener() and Intents with buttons

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/MainActivity.java
@@ -2,7 +2,9 @@ package sanskar.kumar.kumarsanskar.torchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
         torchUsingImage = (Button)findViewById(R.id.button);
         torchUsingButton = (Button)findViewById(R.id.button2);
         torchUsingToggle = (Button)findViewById(R.id.button3);
+        torchUsingImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,UsingImage.class);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        torchUsingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this,UsingButton.class);
+                startActivity(intent2);
+                finish();
+            }
+        });
+        torchUsingToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainActivity.this,UsingToggle.class);
+                startActivity(intent3);
+                finish();
+            }
+        });
+
     }
 }
