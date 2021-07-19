3c0fb00ac2f11b3e692787a75c2fe56f4db70c9a
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-19 20:50:01 +0530

    created and initialized objects of Buttons

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/MainActivity.java
@@ -3,12 +3,17 @@ package sanskar.kumar.kumarsanskar.torchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button torchUsingImage,torchUsingButton,torchUsingToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        torchUsingImage = (Button)findViewById(R.id.button);
+        torchUsingButton = (Button)findViewById(R.id.button2);
+        torchUsingToggle = (Button)findViewById(R.id.button3);
     }
 }
