0e945bee94cded22df30295394bb3ae712d19c1b
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-20 11:59:40 +0530

    used setOnClickListener() method on button

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingImage.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingImage.java
@@ -2,8 +2,10 @@ package sanskar.kumar.kumarsanskar.torchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class UsingImage extends AppCompatActivity {
@@ -17,5 +19,11 @@ public class UsingImage extends AppCompatActivity {
         setContentView(R.layout.activity_using_image);
         powerOn = (ImageButton)findViewById(R.id.imageButton);
         cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
+        powerOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
