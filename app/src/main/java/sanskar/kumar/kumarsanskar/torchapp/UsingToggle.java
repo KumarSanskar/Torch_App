e64d0270ddd18dcf10f963533cd1ce8a62daaf16
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-21 02:43:23 +0530

    created and initialized objects, used .setOnCheckedChangeListener() with toggleButton, defined if else block for when it is checked  and then try- catch block to use flash light

--- app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingToggle.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/torchapp/UsingToggle.java
@@ -1,14 +1,47 @@
 package sanskar.kumar.kumarsanskar.torchapp;
 
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class UsingToggle extends AppCompatActivity {
+    ToggleButton turnOn;
+    CameraManager cameraManager;
+    private boolean camera = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_using_toggle);
+        turnOn = findViewById(R.id.toggleButton);
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        turnOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    try {
+                        String s1 = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(s1, true);
+                        camera = true;
+                    } catch (CameraAccessException e) {
+
+                    }
+
+                }else{
+                    try {
+                        String s2 = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(s2, false);
+                        camera = false;
+                    } catch (CameraAccessException e) {
+
+                    }
+                }
+            }
+        });
+
     }
 }
